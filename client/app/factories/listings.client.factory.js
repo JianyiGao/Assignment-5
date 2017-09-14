aa0a4814eb7af4ef11774d0d2f9374f0c08b7da2
JianyiGao <gaojianyi912@gmail.com>
2017-09-13 21:31:48 -0400

    Url changed

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://cenbootcamp.herokuapp.com/listings');
+        return $http.get('https://cenbootcamp.herokuapp.com/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('https://cenbootcamp.herokuapp.com/listings', listing);
+        return $http.post('https://cenbootcamp.herokuapp.com/api/listings', listing);
       },
 
       read: function(id) {
-        return $http.get('https://cenbootcamp.herokuapp.com/listings' + id);
+        return $http.get('https://cenbootcamp.herokuapp.com/api/listings' + id);
       },
 
       update: function(id, listing) {
-        return $http.put('https://cenbootcamp.herokuapp.com/listings' + id, listing);
+        return $http.put('https://cenbootcamp.herokuapp.com/api/listings' + id, listing);
       },
 
       delete: function(id) {
-        return $http.delete('https://cenbootcamp.herokuapp.com/listings' + id);
+        return $http.delete('https://cenbootcamp.herokuapp.com/api/listings' + id);
       }
     };
 
